f726668fcd99c628b36e12bfd3ba1153ac471ecc
João Alencar <joao.alencar1497@gmail.com>
2022-10-15 01:05:00 -0300

    1 Alteração  - adicionado carregamento da configuração para o dbContext

--- CpmPedidos.Domain/Entities/Cliente.cs
+++ CpmPedidos.Domain/Entities/Cliente.cs
@@ -7,7 +7,6 @@
         public int IdEndereco { get; set; }
         public virtual Endereco Endereco { get; set; }
         public bool Ativo { get; set; }
-
         public virtual List<Pedido> Pedidos { get; set; }
     }
 }
---
+++ CpmPedidos.Repository/Common/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace CpmPedidos.Repository
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var enviromentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT");
+            var fileName = Directory.GetCurrentDirectory() + $"/../CpmPedidos.API/appsettings.{enviromentName}.json";
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(fileName)
+                .Build();
+            var connectionString = configuration.GetConnectionString("App");
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
